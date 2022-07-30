3f3c8099a3de0b20cb15c8067c7c0234d800f79a
Pavel <pavel.filkovskiy@gmail.com>
2022-07-30 13:00:42 +0100

    JavaScript-Core-2-Coursework-Week3

--- JavaScript-Core-2-Coursework-Week3/1-exercises/B-todo-list/script.js
+++ JavaScript-Core-2-Coursework-Week3/1-exercises/B-todo-list/script.js
@@ -1,12 +1,23 @@
 function todoList(todos) {
   // Write your code here...
+  let contain = document.querySelector("#content");
+  let myList = document.createElement("ul");
+
+  contain.appendChild(myList);
+  todos.forEach((element) => {
+    let myUList = document.createElement("li");
+    myList.appendChild(myUList);
+    let myText = document.createElement("p");
+    myUList.appendChild(myText);
+    myText.innerText = `${element.todo}`;
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
--- JavaScript-Core-2-Coursework-Week3/2-projects/1-todo-list/script.js
+++ JavaScript-Core-2-Coursework-Week3/2-projects/1-todo-list/script.js
@@ -2,6 +2,40 @@ function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
+function todoAdd(text) {
+  let ulList = document.querySelector(`ul`);
+  let liList = document.createElement(`li`);
+  liList.classList.add(
+    `list-group-item`,
+    `d-flex`,
+    `justify-content-between`,
+    `align-items-center`
+  );
+  liList.innerText = text;
+  let spanElement = document.createElement(`span`);
+  spanElement.classList.add("badge", "bg-primary", "rounded-pill");
+  let iElementCheck = document.createElement(`i`);
+  iElementCheck.classList.add("fa", "fa-check");
+  let iElementTrash = document.createElement(`i`);
+  iElementTrash.classList.add("fa", "fa-trash");
+  ulList.appendChild(liList);
+  liList.appendChild(spanElement);
+  spanElement.appendChild(iElementCheck);
+  spanElement.appendChild(iElementTrash);
+  iElementCheck.addEventListener(`click`, checkToDo);
+  iElementTrash.addEventListener(`click`, deleteToDo);
+}
+function checkToDo(event) {
+  let iElementCheck = event.target;
+  let liParentNode = iElementCheck.parentNode.parentNode;
+  liParentNode.style.textDecoration =
+    liParentNode.style.textDecoration === `none` ? "line-through" : `none`;
+}
+function deleteToDo(event) {
+  let iElementTrash = event.target;
+  let liParentNode = iElementTrash.parentNode.parentNode;
+  liParentNode.remove();
+}
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
@@ -9,6 +43,9 @@ let todos = [
   { task: "Wash the dishes", completed: false },
   { task: "Do the shopping", completed: false },
 ];
+todos.forEach((element) => {
+  todoAdd(element.task);
+});
 
 populateTodoList(todos);
 
@@ -16,6 +53,8 @@ populateTodoList(todos);
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
+  let textValue = document.getElementById(`todoInput`);
+  todoAdd(textValue.value);
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 }
 
